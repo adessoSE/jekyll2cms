6f85bd012ceae342b6f5181f79fdd29e26bde4e6
David Klassen <daviddd.kl@gmail.com>
2017-05-08 11:13:16 +0200

    Refactor current state
    - add logging
    - add different controller for git and jekyll cmds
    - add CmdController to deligate cmds to controller

--- src/main/java/de/adesso/controller/JekyllController.java
+++ src/main/java/de/adesso/controller/JekyllController.java
@@ -1,7 +1,8 @@
-package de.adesso;
+package de.adesso.controller;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
@@ -11,21 +12,24 @@ import java.io.IOException;
 @Controller
 public class JekyllController {
 
-    @Value("${repository.local.path}")
-    private String localRepoPath;
+    private final static Logger LOGGER = Logger.getLogger(JekyllController.class);
 
-    public void runJekyll() {
+    @Value("${repository.local.path}")
+    private String LOCAL_REPO_PATH;
+
+    public void runJekyllBuild() {
+        LOGGER.info("Starting: Jekyll build");
 
         String line = "C:/tools/ruby23/bin/jekyll.bat build";
         CommandLine cmdLine = CommandLine.parse(line);
         DefaultExecutor executor = new DefaultExecutor();
-        executor.setWorkingDirectory(new File(localRepoPath));
+        executor.setWorkingDirectory(new File(LOCAL_REPO_PATH));
 
         try {
             int exitValue = executor.execute(cmdLine);
-            System.out.println("// " + exitValue);
+            LOGGER.info("Jekyll build exited with value: " + exitValue);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("Error while executing jekyll build.", e);
         }
 
     }
--- src/main/java/de/adesso/controller/RepoController.java
+++ src/main/java/de/adesso/controller/RepoController.java
@@ -1,5 +1,6 @@
-package de.adesso;
+package de.adesso.controller;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
@@ -13,32 +14,36 @@ import java.io.IOException;
 @Controller
 public class RepoController {
 
-    @Value("${repository.local.path}")
-    private String localRepoPath;
+    private final static Logger LOGGER = Logger.getLogger(RepoController.class);
 
+    @Value("${repository.local.path}")
+    private String LOCAL_REPO_PATH;
     @Value("${repository.remote.url}")
-    private String remoteRepoUrl;
+    private String REMOTE_REPO_URL;
 
     private Repository localRepo;
     private Git git;
 
     public void initLocalRepo() {
+        LOGGER.info("Starting: Git init local repository");
+
         try {
-            localRepo = new FileRepository(localRepoPath + "/.git");
+            localRepo = new FileRepository(LOCAL_REPO_PATH + "/.git");
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("Error while initialising local git repository", e);
         }
         git = new Git(localRepo);
     }
 
     public void cloneRemoteRepo() {
+        LOGGER.info("Starting: Git clone remote repository");
         try {
             Git.cloneRepository()
-                    .setURI(remoteRepoUrl)
-                    .setDirectory(new File(localRepoPath))
+                    .setURI(REMOTE_REPO_URL)
+                    .setDirectory(new File(LOCAL_REPO_PATH))
                     .call();
         } catch (GitAPIException e) {
-            e.printStackTrace();
+            LOGGER.error("Error while cloning remote git respository", e);
         }
     }
 }
