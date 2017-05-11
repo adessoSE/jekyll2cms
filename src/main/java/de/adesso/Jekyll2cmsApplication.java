4a51df4a94b920050b57024e69af97379aab3f5b
David Klassen <daviddd.kl@gmail.com>
2017-05-11 14:50:55 +0200

    Revert test comment

--- src/main/java/de/adesso/Jekyll2cmsApplication.java
+++ src/main/java/de/adesso/Jekyll2cmsApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Jekyll2Cms application class
- *
- */
 @SpringBootApplication
 public class Jekyll2cmsApplication implements ApplicationRunner {
 
