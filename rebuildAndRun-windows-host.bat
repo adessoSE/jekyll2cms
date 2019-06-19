4359a1de9dbdae4ceb028e84a1d450c1bdc195c1
SilasMahler <simah002@stud.fh-dortmund.de>
2019-06-19 15:20:47 +0200

    add call

--- rebuildAndRun-windows-host.bat
+++ rebuildAndRun-windows-host.bat
@@ -1,4 +1,5 @@
 call gradlew build
 call docker build --rm -f "Dockerfile" -t jekyll2cms:1.0.6 .
+call rd /s /q "./repo"
 call docker run --rm -it  -v %cd%:/srv/jekyll -p 127.0.0.1:4000:4000 jekyll2cms:1.0.6
 
